af68a78eaaad66cfaffdf94e1572146671fb4cc5
NIKHIL YADAV <nikhilyadav835402@gmail.com>
2026-03-28 23:57:21 +0530

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Cname="nikhil"
 const age=23;
-//first commit
+//first commit 
 console.log(Cname);
 console.log(age);
+//second commit in master
+ const account_number=3478349;
+ let account_balance=0;
+ console.log(account_balance);
+  console.log(account_number);
